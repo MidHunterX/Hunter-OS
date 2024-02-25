6c67b79b27cda6400cf9be0ccea58e984c397efe
MidHunterX <midhunterx@gmail.com>
2024-02-25 20:00:55 +0530

    ⚙️ Updated Command list to use CSV

--- Mid_Hunter/scripts/archchan/get_command_list.sh
+++ Mid_Hunter/scripts/archchan/get_command_list.sh
@@ -11,61 +11,19 @@ CMD=$BLU # Command
 STR=$GRN # Strings
 INT=$PNK # Integer
 
-# Define the list of keywords and corresponding commands
-keywords=(
-  'Oopsie.. Select nothing'
-  '[wpctl] Show audio devices'
-  '[tlp] Disable Conservation Mode - Set to fullcharge temporarily'
-  '[tlp] Enable Conservation Mode - Set charging limit @60%'
-  '[pacman] Remove unwanted depencies'
-  '[pacman] Show unwanted depencies'
-  '[aft-mtp-mount] Mount Android Device'
-  '[curlftpfs] Mount FTP Device'
-  '[umount] Unmount Device/Partition'
-  '[nmcli] Wifi Connect'
-  '[nmcli] Wifi Disable'
-  '[nmcli] Wifi Enable'
-  '[nmcli] Network Disable'
-  '[nmcli] Network Enable'
-  '[ls] List installed themes'
-  '[systemctl] Show running background services'
-)
-commands=(
-  ''
-  'wpctl status'
-  'sudo tlp fullcharge'
-  'sudo tlp setcharge'
-  'sudo pacman -R $(pacman -Qdtq)'
-  'pacman -Qdtq'
-  'aft-mtp-mount ~/mnt'
-  'curlftpfs ftphostname ~/mnt'
-  'sudo umount ~/mnt'
-  'nmcli device wifi list '
-  'nmcli radio wifi off'
-  'nmcli radio wifi on'
-  'nmcli networking off'
-  'nmcli networking on'
-  'ls /usr/share/themes/'
-  'systemctl status'
-)
-comments=(
-  'Go on... Have a nice day, master ;D'
-  "Set default device using, ${CMD}wpctl set-default <${INT}number${CMD}>. ${CMT}Source = Microphone and Sink = Sound."
-  'You can set default charging mode at: /etc/tlp.conf'
-  'You can set default charging mode at: /etc/tlp.conf'
-  'Done'
-  'These are the things you gotta yeet!'
-  'Done'
-  'Done'
-  'Done'
-  'You can connect with, nmcli device wifi connect <SSID>'
-  'Done'
-  'Done'
-  'Done'
-  'Done'
-  "You can set theme using, ${CMD}gsettings set org.gnome.desktop.interface gtk-theme '${STR}theme_name${CMD}'"
-  'Done'
-)
+
+# Initialize arrays
+keywords=()
+commands=()
+comments=()
+
+# Read a CSV file and process each line
+while IFS=',' read -r col1 col2 col3; do
+    keywords+=("$col1")
+    commands+=("$col2")
+    comments+=("$col3")
+done < "get_command_list.csv"
+
 
 # Prompt for input
 usr_keyword=$(printf "%s\n" "${keywords[@]}" | fzf --prompt 'Select Command: ')
@@ -74,10 +32,10 @@ usr_keyword=$(printf "%s\n" "${keywords[@]}" | fzf --prompt 'Select Command: ')
 # Find the index of the selected keyword
 index=-1
 for i in "${!keywords[@]}"; do
-    if [[ "${keywords[$i]}" == "$usr_keyword" ]]; then
-        index=$i
-        break
-    fi
+  if [[ "${keywords[$i]}" == "$usr_keyword" ]]; then
+    index=$i
+    break
+  fi
 done
 
 if [[ index -ne -1 ]]; then
