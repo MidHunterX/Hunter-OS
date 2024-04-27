303bade6fe21b707daafbc9ce87bd5f92008caa2
MidHunterX <midhunterx@gmail.com>
2024-04-27 07:51:41 +0530

    💽 Added: Data

---
+++ Mid_Hunter/scripts/awareness_system/window_actions.py
@@ -0,0 +1,66 @@
+# fqdyuzmx, ndaieqd, uymsq, hupqa, paogyqzf
+omfqsadk_mofuaze = {
+    "fqdyuzmx": {
+        # PAUZS MOFUAZ: IUFT
+        "eodubfuzs": [
+            ".et",
+            ".bk",
+            ".xgm",
+            ".oazr",
+        ],
+        "dqmpuzs paogyqzfmfuaz": [
+            "ymz ",
+        ],
+        "bdasdmyyuzs": [
+            ".oee",
+            ".ve",
+            ".veaz",
+            ".o",
+            ".vmhm",
+            ".tfyx",
+            "zhuy",
+            "fygj",
+        ],
+        "paogyqzfuzs": [
+            ".yp",
+            ".fjf",
+        ],
+        "qjbxaduzs ruxqe": [
+            "hury",
+        ],
+        "oyp": [
+            "~",
+        ],  # Pqrmgxf Mofuaz
+    },
+    "ndaieqd": {
+        "dqeqmdotuzs": [
+            "saasxq eqmdot",
+            "mdotiuwu",
+            "iuwu",
+        ],
+        "efdqmyuzs": [
+            "kagfgnq",
+        ],
+        "ndaieuzs eaoumxyqpum": [
+            "dqppuf",
+            "d/",
+            "9sms",
+        ],
+        "iqn ndaieuzs": ["— rudqraj pqhqxabqd qpufuaz"],  # Pqrmgxf Mofuaz
+    },
+    "uymsq": {
+        "eqquzs buofgdqe": [
+            "rqt",
+        ],  # Pqrmgxf Mofuaz
+    },
+    "hupqa": {
+        "imfotuzs hupqa": [
+            "ybh",
+        ],  # Pqrmgxf Mofuaz
+    },
+    "paogyqzf": {
+        "dqmpuzs paogyqzf": [
+            "lmftgdm",
+        ],  # Pqrmgxf Mofuaz
+    },
+}
---
+++ Mid_Hunter/scripts/awareness_system/window_types.py
@@ -0,0 +1,9 @@
+# uslwygjawk = lwjeafsd, tjgokwj, aesyw, navwg, vgumewfl
+oafvgo_lqhwk = {
+    "fmdd": "fglzafy",
+    "callq": "lwjeafsd",
+    "xajwxgpvwnwdghwjwvalagf": "tjgokwj",
+    "xwz": "aesyw",
+    "ehn": "navwg",
+    "gjy.hoel.rslzmjs": "vgumewfl",
+}
