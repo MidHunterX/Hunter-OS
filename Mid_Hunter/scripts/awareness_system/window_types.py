0b33522a7dac4de25166cf227278a82142efa0e6
MidHunterX <midhunterx@gmail.com>
2024-04-27 13:07:47 +0530

    ⚙️ Delet: assume-unchanged files

--- Mid_Hunter/scripts/awareness_system/window_types.py
+++
@@ -1,9 +0,0 @@
-# uslwygjawk = lwjeafsd, tjgokwj, aesyw, navwg, vgumewfl
-oafvgo_lqhwk = {
-    "fmdd": "fglzafy",
-    "callq": "lwjeafsd",
-    "xajwxgpvwnwdghwjwvalagf": "tjgokwj",
-    "xwz": "aesyw",
-    "ehn": "navwg",
-    "gjy.hoel.rslzmjs": "vgumewfl",
-}
