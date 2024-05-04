dd02e4bc11cf2a60ef744170edaa4ee38b3fd3c5
MidHunterX <midhunterx@gmail.com>
2024-05-04 17:01:42 +0530

    ⚙️ Added: Package Dependencies

--- Mid_Hunter/scripts/fetch.sh
+++ Mid_Hunter/scripts/fetch.sh
@@ -3,7 +3,11 @@
 # █▀ █▀▀ ▀█▀ ▀█▀ █ █▄░█ █▀▀ █▀
 # ▄█ ██▄ ░█░ ░█░ █ █░▀█ █▄█ ▄█
 # ============================
-ICO='  '
+# ICO='  '
+# ICO='🯇🯅🯈 '
+# ICO='⣿⣿⣿ '
+# ICO='▜▙  '
+ICO='█ '
 # 1=100% 2=50% 3=33% 4=25% 5=20%
 BAR_SCALING=5
 # Some Icons: ━ ≡ = ◍◌ ▮▯
@@ -17,6 +21,7 @@ BAR_REST='━'
 BLK='\033[1;30m' RED='\033[1;31m' GRN='\033[1;32m' YLO='\033[1;33m'
 BLU='\033[1;34m' PNK='\033[1;35m' CYN='\033[1;36m' WHT='\033[1;37m'
 DEF='\033[0;39m' RESET='\033[0;0m'
+U='\033[4m' # Underline
 R=${RESET}
 X=${BLU} # Accent 1
 Y=${YLO} # Accent 2
@@ -33,6 +38,10 @@ V=${YLO} # Values
 uptime=$(uptime -p)
 read -r hr hr_label min min_label <<< "$(awk -F'( |,)+' '{print $2,$3,$4,$5}' <<< "$uptime")"
 UPTIME="${V}${hr}${R} ${hr_label} ${V}${min}${R} ${min_label}"
+
+PAK_EXPLICIT=$(pacman -Qeq | wc -l)
+PAK_DEPENDEN=$(pacman -Qdq | wc -l)
+PAK_TOTAL=$((PAK_EXPLICIT + PAK_DEPENDEN))
 
 BAR_FRACTION=${BAR_SCALING}
 ROUNDOFF=${BAR_SCALING}
@@ -91,19 +100,18 @@ fi
 # █▀█ █░█ ▀█▀ █▀█ █░█ ▀█▀
 # █▄█ █▄█ ░█░ █▀▀ █▄█ ░█░
 echo " "
-echo -e "  ${X}     ${Y}o. ${X}      ${Y} .o${X}     ${R}   ${H}midhunter${R}@${H}Flex-5"
-echo -e "  ${X},    ${Y}yyo${X}      ${Y}oyy${X}    ,${R}   ${L}----------------"
-echo -e "  ${X}oNo  ${Y}yyy${X}      ${Y}yyy${X}  oNo${R}   ${B}OS:${R} Hunter OS"
-echo -e "  ${X}oMMNs${Y}yyy${X}      ${Y}yyy${X}sNMMo${R}   ${B}Host:${R} 82HU IdeaPad Flex 5 14ALC05"
-echo -e "  ${X}oMMMM${Y}yyy${X}      ${Y}yyy${X}MMMMo${R}   ${B}Datetime:${R} $(date +"${V}%d${R} %b %Y, ${V}%H:%M${R} %a")"
-echo -e "  ${X}oMMhd${Y}yyy${X}y.  .y${Y}yyy${X}yyMMo${R}   ${B}Uptime:${R} ${UPTIME}"
-echo -e "  ${X}oMMo ${Y}yyy${X}MMyyMM${Y}yyy${X} oMMo${R}"
-echo -e "  ${X}oMMo ${Y}yyy${X}sNMMNo${Y}yyy${X} oMMo${R}   ${H}Device Details:${R}"
-echo -e "  ${X}oMMo ${Y}yyy${X} '++' ${Y}yyy${X} oMMo${R}   ${L}---------------"
-echo -e "  ${X}oMMo ${Y}yyy${X}      ${Y}yyy${X} oMMo${R}   ${B}CPU:${R} AMD Ryzen 5 5500U"
-echo -e "  ${X}oMMo ${Y}oyy${X}      ${Y}yyo${X} oMMo${R}   ${B}GPU:${R} AMD Radeon RX Vega 7"
-echo -e "  ${X}oMMo ${Y} *o${X}      ${Y}o* ${X} oMMo${R}   ${B}SSD:${R} ${COL_SSD}$SSD_BAR${BLK}$SSD_REST_BAR${R} $SSD_PERCENT%${R}"
-echo -e "  ${X}oMMo ${Y}   ${X}      ${Y}   ${X} oMMo${R}   ${B}RAM:${R} ${COL_RAM}$RAM_BAR${BLK}$RAM_REST_BAR${R} $RAM_VALUE MB${R}"
-echo -e "  ${X}:NMo ${Y}   ${X}      ${Y}   ${X} oMN:${R}   ${B}BAT:${R} ${COL_BAT}$BAT_BAR${BLK}$BAT_REST_BAR${R} $BAT_PERCENT%${R}"
-echo -e "  ${X}  o+ ${Y}   ${X}      ${Y}   ${X} +o  ${R}"
-echo -e "                           ${RED}${ICO}${GRN}${ICO}${YLO}${ICO}${BLU}${ICO}${PNK}${ICO}${CYN}${ICO}${WHT}${ICO}${BLK}${ICO}${R}"
+echo -e "  ${X}     ${Y}o. ${X}      ${Y} .o${X}     ${R}   ${RED}${ICO}${GRN}${ICO}${YLO}${ICO}${BLU}${ICO}${PNK}${ICO}${CYN}${ICO}${WHT}${ICO}${BLK}${ICO}${R}"
+echo -e "  ${X},    ${Y}yyo${X}      ${Y}oyy${X}    ,${R}"
+echo -e "  ${X}oNo  ${Y}yyy${X}      ${Y}yyy${X}  oNo${R}   ${U}${H}midhunter${WHT}@${H}Flex-5${R}"
+echo -e "  ${X}oMMNs${Y}yyy${X}      ${Y}yyy${X}sNMMo${R}   ${B}OS:${R} Hunter OS"
+echo -e "  ${X}oMMMM${Y}yyy${X}      ${Y}yyy${X}MMMMo${R}   ${B}Host:${R} 82HU IdeaPad Flex 5 14ALC05"
+echo -e "  ${X}oMMhd${Y}yyy${X}y.  .y${Y}yyy${X}yyMMo${R}   ${B}Packages:${R} $PAK_EXPLICIT + $PAK_DEPENDEN = $PAK_TOTAL"
+echo -e "  ${X}oMMo ${Y}yyy${X}MMyyMM${Y}yyy${X} oMMo${R}   ${B}Datetime:${R} $(date +"${V}%d${R} %b %Y, ${V}%H:%M${R} %a")"
+echo -e "  ${X}oMMo ${Y}yyy${X}sNMMNo${Y}yyy${X} oMMo${R}   ${B}Uptime:${R} ${UPTIME}"
+echo -e "  ${X}oMMo ${Y}yyy${X} '++' ${Y}yyy${X} oMMo${R}"
+echo -e "  ${X}oMMo ${Y}yyy${X}      ${Y}yyy${X} oMMo${R}   ${U}${H}Device Details:${R}"
+echo -e "  ${X}oMMo ${Y}oyy${X}      ${Y}yyo${X} oMMo${R}   ${B}CPU:${R} AMD Ryzen 5 5500U"
+echo -e "  ${X}oMMo ${Y} *o${X}      ${Y}o* ${X} oMMo${R}   ${B}GPU:${R} AMD Radeon RX Vega 7"
+echo -e "  ${X}oMMo ${Y}   ${X}      ${Y}   ${X} oMMo${R}   ${B}SSD:${R} ${COL_SSD}$SSD_BAR${BLK}$SSD_REST_BAR${R} $SSD_PERCENT%${R}"
+echo -e "  ${X}:NMo ${Y}   ${X}      ${Y}   ${X} oMN:${R}   ${B}RAM:${R} ${COL_RAM}$RAM_BAR${BLK}$RAM_REST_BAR${R} $RAM_VALUE MB${R}"
+echo -e "  ${X}  o+ ${Y}   ${X}      ${Y}   ${X} +o  ${R}   ${B}BAT:${R} ${COL_BAT}$BAT_BAR${BLK}$BAT_REST_BAR${R} $BAT_PERCENT%${R}"
