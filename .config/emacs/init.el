e58ce2f69fd7ff4708de5cff68c5459373d97e87
MidHunterX <midhunterx@gmail.com>
2024-05-11 22:56:43 +0530

    🍔 Added: Colorschemes and Statusline

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -11,8 +11,11 @@
 ;; ▄█ ██▄ ░█░ ░█░ █ █░▀█ █▄█ ▄█
 ;; ============================
 (global-hl-line-mode t)                         ; Highligh Current Line
-(load-theme `tango-dark)                        ; Colorscheme
-(set-face-attribute 'default nil :height 140)   ; Font Size
+;; (load-theme `tango-dark)                        ; Colorscheme
+(set-face-attribute 'default nil :height 150)   ; Font Size
+(set-frame-parameter nil 'alpha-background 70)
+(add-to-list 'default-frame-alist '(alpha-background . 70))
+
 
 ;; ORG Mode Stuff
 (defvar dw/fixed-pitch-font "JetBrains Mono"
@@ -60,6 +63,30 @@
 (require 'evil)
 (evil-mode 1)
 
+;; COLORSCHEME
+(unless (package-installed-p 'catppuccin-theme)
+  (package-install 'catppuccin-theme))
+(load-theme 'catppuccin :no-confirm)
+(setq catppuccin-flavor 'mocha)
+(catppuccin-reload)
+
+;; DOOM MODELINE
+(unless (package-installed-p 'doom-modeline)
+  (package-install 'doom-modeline))
+(require 'doom-modeline)
+(doom-modeline-mode 1)
+
+
 ;; UNTOUCHABLES
-(custom-set-variables '(package-selected-packages '(evil)))
-(custom-set-faces)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
