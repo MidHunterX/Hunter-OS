63517446c99d28a313bff79444a1467a6a4caee0
MidHunterX <midhunterx@gmail.com>
2024-05-12 14:38:51 +0530

    ⚙️ Changed: Naming Convention

--- Mid_Hunter/scripts/sys_windows.sh
+++ Mid_Hunter/scripts/sys_windows.sh
@@ -1,9 +1,8 @@
 # Reboot directly to Windows
-reboot_to_windows (){
+set_GRUB_default_entry (){
   # eg: 'Windows Boot Manager (on /dev/nvme0n1p1)'
   windows_title=$(sudo grep -i windows /boot/grub/grub.cfg | cut -d "'" -f 2)
   sudo grub-reboot "$windows_title"
-  sudo reboot
 }
 
 
@@ -61,4 +60,5 @@ display_center_colors "${RESET}"
 
 echo -e "\n\n${GRN}"
 space_center
-reboot_to_windows
+set_GRUB_default_entry
+sudo reboot
