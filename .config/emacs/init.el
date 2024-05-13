927d6c207872a502230f896c0443313b0ae400d5
MidHunterX <midhunterx@gmail.com>
2024-05-13 08:57:37 +0530

    🍔 Added: More Dashboard Customizations

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -119,6 +119,7 @@
   (package-install 'catppuccin-theme))
 (load-theme 'catppuccin :no-confirm)
 (setq catppuccin-flavor 'mocha)
+(catppuccin-set-color 'base "#262626")
 (catppuccin-reload)
 
 ;; DOOM MODELINE
@@ -138,6 +139,25 @@
 (setq dashboard-banner-logo-title "Welcome to Emacs")
 (setq dashboard-center-content t)
 (setq dashboard-vertically-center-content t)
+(setq dashboard-show-shortcuts t)
+(setq dashboard-items '((recents   . 5)
+                        (projects  . 5)
+                        (agenda    . 5)))
+(setq dashboard-navigation-cycle t)
+(setq dashboard-display-icons-p t)
+(setq dashboard-icon-type 'nerd-icons)
+(setq dashboard-startupify-list '(dashboard-insert-banner
+                                  dashboard-insert-newline
+                                  dashboard-insert-banner-title
+                                  dashboard-insert-newline
+                                  dashboard-insert-navigator
+                                  dashboard-insert-newline
+                                  dashboard-insert-init-info
+                                  dashboard-insert-items
+                                  dashboard-insert-newline
+                                  dashboard-insert-footer))
+(setq dashboard-set-heading-icons t)
+(setq dashboard-set-file-icons t)
 (require 'dashboard)
 (dashboard-setup-startup-hook)
 
