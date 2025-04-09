2237cdc11e3ce03345dac784530ded9ae8d2161d
MidHunterX <midhunterx@gmail.com>
2025-04-09 11:12:53 +0530

    🚚 Move Awareness System
    Signed-off-by: MidHunterX <midhunterx@gmail.com>

--- Mid_Hunter/scripts/awareness_system/data/window_types.py
+++
@@ -1,9 +0,0 @@
-# categories = terminal, browser, image, video, document
-window_types = {
-    "null": "nothing",
-    "kitty": "terminal",
-    "firefoxdeveloperedition": "browser",
-    "feh": "image",
-    "mpv": "video",
-    "org.pwmt.zathura": "document",
-}
