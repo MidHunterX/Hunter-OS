47eecc5895b5fe5aec0661b7384783abc16d1cbe
MidHunterX <midhunterx@gmail.com>
2025-10-26 11:06:27 +0530

    feat(ai_theme): add wallpaper average color detection

--- Mid_Hunter/scripts/ai_theme.sh
+++ Mid_Hunter/scripts/ai_theme.sh
@@ -2,26 +2,28 @@
 # Theme Auto Switcher based on Wallpaper Brightness
 # Requires: ImageMagick, swww (for wallpaper), gsettings
 
-print_logs=false
-
 # Hyprland Preference
 light_brightness=0.2
 dark_brightness=0.6
 light_contrast=0.5
 dark_contrast=0.8
 
+print_logs=false
+
+[[ "$1" == "--log" ]] && print_logs=true
+
 log() {
   local grn='\033[1;32m'
   local rst='\033[0;0m'
   if $print_logs; then
-    echo -e "[${grn}$(date +'%H:%M:%S')${rst}] $1"
+    echo -e "[${grn}$(date +'%H:%M:%S')${rst}] $1${rst}"
   fi
 }
 
 error() {
   local red='\033[1;31m'
   local rst='\033[0;0m'
-  echo -e "[${red}$(date +'%H:%M:%S')${rst}] $1"
+  echo -e "[${red}$(date +'%H:%M:%S')${rst}] $1${rst}"
 }
 
 # Linear Interpolation (lerp)
@@ -73,5 +75,36 @@ contrast=$(lerp "$g_norm" "$dark_contrast" "$light_contrast")
 log "☀️ Brightness: $brightness"
 log "🌗 Contrast: $contrast"
 
-hyprctl keyword decoration:blur:brightness $brightness >/dev/null
-hyprctl keyword decoration:blur:contrast $contrast >/dev/null
+hyprctl keyword decoration:blur:brightness "$brightness" >/dev/null
+hyprctl keyword decoration:blur:contrast "$contrast" >/dev/null
+
+# Step 5: AVERAGE COLOR
+hex_to_ansi() {
+  local hex="${1#'#'}" # remove leading #
+  local bg=$2
+  local r=$((16#${hex:0:2}))
+  local g=$((16#${hex:2:2}))
+  local b=$((16#${hex:4:2}))
+  if [[ -n "$bg" ]]; then
+    # Set text color
+    local brightness=$(((299 * r + 587 * g + 114 * b) / 1000))
+    ((brightness < 128)) && printf '\033[38;2;255;255;255m' || printf '\033[38;2;0;0;0m'
+    # Set background color
+    printf '\033[48;2;%d;%d;%dm' "$r" "$g" "$b"
+  else
+    # Set text color only
+    printf '\033[38;2;%d;%d;%dm' "$r" "$g" "$b"
+  fi
+}
+
+if $print_logs; then
+  TMP_IMG="/tmp/theme_color.jpeg"
+  magick "$WALLPAPER_PATH" -resize 1x1 "$TMP_IMG" 2>/dev/null
+  hex_code=$(magick "$TMP_IMG" txt: | awk '/#\w+/{print $3}')
+  ansi_color=$(hex_to_ansi "$hex_code" bg_pls)
+
+  log "🎨 Color: ${ansi_color}${hex_code}"
+
+  # hex_number=${hex_code:1}
+  # hyprctl keyword general:col.inactive_border "rgb(${hex_number})" >/dev/null
+fi
