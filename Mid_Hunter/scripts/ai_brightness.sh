ebe5e930b954babb2f7c2c2cf50aa48872fad35d
MidHunterX <midhunterx@gmail.com>
2025-10-31 18:01:15 +0530

    fix(brs): leaking variables fixes

--- Mid_Hunter/scripts/ai_brightness.sh
+++ Mid_Hunter/scripts/ai_brightness.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # "Check if script is already running"; $0 == filename; $$ == PID
-if [[ `pgrep -f $0` != "$$" ]]; then
+if [[ $(pgrep -f "$0") != "$$" ]]; then
   echo "Another instance of shell already exist!"
   exit
 fi
@@ -12,17 +12,17 @@ INTERVALS=15
 CONFIG_FILE=~/Mid_Hunter/scripts/set_brightness.conf
 source ${CONFIG_FILE}
 
-while [[ 1 ]]; do
+while true; do
   HOUR=$(date +"%H")
   MINUTE=$(date +"%M")
-  MINUTE_CEIL=$(( (INTERVALS - (10#$MINUTE % INTERVALS) + 10#$MINUTE) ))
+  MINUTE_CEIL=$((INTERVALS - (10#$MINUTE % INTERVALS) + 10#$MINUTE))
   # Jump to next hour if 60
   if [[ $MINUTE_CEIL == 60 ]]; then
     HOUR=$((10#$HOUR + 1))
     printf -v HOUR "%02d" $HOUR
     MINUTE_CEIL=00
   fi
-  WAIT=$((($INTERVALS - (10#$MINUTE % $INTERVALS))*60))
+  WAIT=$(((INTERVALS - (10#$MINUTE % INTERVALS)) * 60))
 
   STORED_VARIABLE="BR_${HOUR}_${MINUTE_CEIL}"
   STORED_BRIGHTNESS=${!STORED_VARIABLE}
@@ -30,7 +30,7 @@ while [[ 1 ]]; do
   echo "$STORED_VARIABLE = $STORED_BRIGHTNESS"
 
   # Set Brightness
-  brillo -u 5000000 -S ${STORED_BRIGHTNESS}
+  brillo -u 5000000 -S "${STORED_BRIGHTNESS}"
 
   echo $WAIT
   sleep $WAIT
